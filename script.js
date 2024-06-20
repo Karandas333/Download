45f2d9e5d5637b18abb94399325e41f46e74af41
Karandas333 <146736956+Karandas333@users.noreply.github.com>
2024-06-20 18:38:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let btn = document.querySelector('.btn');
+let text = document.querySelector('#text');
+let bg = document.querySelector('#bg');
+
+btn.addEventListener("click",()=>{
+  gsap.to(bg,{
+    x:'0%',
+    delay:0.4,
+    duration: 3
+  })
+  gsap.to(text,{
+    color:'#fff',
+    duration:1.5,
+    delay:0.4
+  })
+})
