e4365770055cc8f80813c4ac775d945975e7d36e
Karandas333 <146736956+Karandas333@users.noreply.github.com>
2024-06-20 19:35:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 let btn = document.querySelector('.btn');
 let text = document.querySelector('#text');
 let bg = document.querySelector('#bg');
-let err = document.querySelector('h1');
 
 btn.addEventListener("click", function(){
-  err.innerText = 'Downloading....';
-  gsap.to(bg,{
-    x:0,
+  
+  gsap.from(bg,{
+    x:'-100%',
     delay:0.4,
     duration: 3
   })
   gsap.to(text,{
     color:'#fff',
-    duration:1.5,
+    duration:2,
     delay:0.4
   })
 })
